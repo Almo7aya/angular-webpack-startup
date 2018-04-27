985bcd44ad740d386043a3583ae999c2d2f21ebd
Ali Almohaya <AliAlmohaya@gmail.com>
2018-04-27 10:44:44 +0300

    add angular init files

---
+++ src/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    template: `
+        <h1>It Works</h1>
+    `
+})
+export default class AppComponent {
+
+}
---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule, Component } from '@angular/core';
+import AppComponent from './app.component';
+
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent]
+})
+export default class AppModule {
+
+}
