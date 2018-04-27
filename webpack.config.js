a14816129509115cffb6b396e613ae7f24ecab49
Ali Almohaya <AliAlmohaya@gmail.com>
2018-04-27 10:44:28 +0300

    add main configs

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: [
+        path.join(__dirname, 'src/main.ts')
+    ],
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.ts']
+    },
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader'
+            }
+        ]
+    }
+};
